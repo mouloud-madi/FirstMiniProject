af5cfc592a2362ae40ebf28137bc924a86221a37
mouloudmadi <mouloutbk@gmail.com>
2020-04-13 14:41:00 +0100

    Add the function with the name (addNewTask) to display the todo list order by latest

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function addBtn() {
     var task = document.getElementById("taskValue").value;
     //if not empty and example validation
     if (task.length > 3 && /\S/.test(task)) {
+        addNewTask(task);
         document.getElementById("taskValue").value = "";
         document.getElementById("errMessage").innerHTML = "";
         document.getElementById("taskValue").classList.remove('is-invalid');
@@ -12,4 +13,15 @@ function addBtn() {
         document.getElementById("errMessage").innerHTML = "This field is required";
         document.getElementById("taskValue").classList.add('is-invalid');
     }
+}
+
+function addNewTask(element) {
+    var list = document.getElementById("todoList"),
+        todoItem = document.createElement('div'),
+        newItem = document.createElement("li");
+    newItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    newItem.innerText = element;
+    newItem.appendChild(todoItem);
+    //insert new todo before the first element
+    list.insertBefore(newItem, list.childNodes[0]);
 }
